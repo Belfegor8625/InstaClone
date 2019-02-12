3db77dade15a0c8d3917bc2757704a521ca81fe1
Belfegor17 <barlew94@o2.pl>
2019-02-12 11:46:05 +0100

    Rework in SignUp activity Removed redundant activities

--- app/src/main/java/com/bartoszlewandowski/instaclone/SignUp.java
+++ app/src/main/java/com/bartoszlewandowski/instaclone/SignUp.java
@@ -1,23 +1,16 @@
 package com.bartoszlewandowski.instaclone;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import com.parse.FindCallback;
-import com.parse.GetCallback;
-import com.parse.Parse;
 import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
-import com.parse.SaveCallback;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
-
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -26,24 +19,12 @@ import butterknife.OnClick;
 public class SignUp extends AppCompatActivity {
 
     //butterknife implementation
-    @BindView(R.id.btnSave)
-    Button btnSave;
-    @BindView(R.id.edtName)
-    EditText edtName;
-    @BindView(R.id.edtPunchSpeed)
-    EditText edtPunchSpeed;
-    @BindView(R.id.edtPunchPower)
-    EditText edtPunchPower;
-    @BindView(R.id.edtKickSpeed)
-    EditText edtKickSpeed;
-    @BindView(R.id.edtKickPower)
-    EditText edtKickPower;
-    @BindView(R.id.txtGetData)
-    TextView txtGetData;
-    @BindView(R.id.btnGetAllData)
-    Button btnGetAllData;
-    @BindView(R.id.btnTransition)
-    Button btnTransition;
+    @BindView(R.id.edtSignUpEmail)
+    EditText edtEmail;
+    @BindView(R.id.edtSignUpUsername)
+    EditText edtUsername;
+    @BindView(R.id.edtSignUpPassword)
+    EditText edtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,82 +35,29 @@ public class SignUp extends AppCompatActivity {
     }
 
 
-    @OnClick(R.id.btnSave)
-    public void btnSaveClick(View v) {
-
-        try {
-            final ParseObject kickboxer = new ParseObject("kickboxer");
-            kickboxer.put("name", edtName.getText().toString());
-            kickboxer.put("punchSpeed", Integer.parseInt(edtPunchSpeed.getText().toString()));
-            kickboxer.put("punchPower", Integer.parseInt(edtPunchPower.getText().toString()));
-            kickboxer.put("kickSpeed", Integer.parseInt(edtKickSpeed.getText().toString()));
-            kickboxer.put("kickPower", Integer.parseInt(edtKickPower.getText().toString()));
-            kickboxer.saveInBackground(new SaveCallback() {
-                @Override
-                public void done(ParseException e) {
-                    if (e == null) {
-                        FancyToast.makeText(SignUp.this, kickboxer.get("name") + " is saved",
-                                FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
-                    } else {
-                        FancyToast.makeText(SignUp.this, e.getMessage(),
-                                FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
-                    }
-                }
-            });
-        } catch (Exception e) {
-            FancyToast.makeText(SignUp.this, e.getMessage(),
-                    FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
-        }
-    }
-
-
-    @OnClick(R.id.txtGetData)
-    public void txtGetDataClicked(View v) {
-        ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery("kickboxer");
-        parseQuery.getInBackground("XrDUukE4kY", new GetCallback<ParseObject>() {
+    @OnClick(R.id.btnSignUp)
+    public void btnSignUpClick(View v) {
+        final ParseUser parseUser = new ParseUser();
+        parseUser.setEmail(edtEmail.getText().toString());
+        parseUser.setUsername(edtUsername.getText().toString());
+        parseUser.setPassword(edtPassword.getText().toString());
+        parseUser.signUpInBackground(new SignUpCallback() {
             @Override
-            public void done(ParseObject object, ParseException e) {
-                if (object != null && e == null) {
-                    String data = object.get("name") +
-                            "\nPunch Speed: " + object.get("punchSpeed") +
-                            "\nPunch Power: " + object.get("punchPower") +
-                            "\nKick Speed: " + object.get("kickSpeed") +
-                            "\nKick Power: " + object.get("kickPower");
-                    txtGetData.setText(data);
-                }
-            }
-        });
-    }
-
-    @OnClick(R.id.btnGetAllData)
-    public void getAllData(View v) {
-        ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery("kickboxer");
-
-        parseQuery.findInBackground(new FindCallback<ParseObject>() {
-            @Override
-            public void done(List<ParseObject> objects, ParseException e) {
+            public void done(ParseException e) {
                 if (e == null) {
-                    if (objects.size() > 0) {
-                        String allKickBoxers = "";
-                        for (ParseObject parseObject : objects) {
-                            allKickBoxers = allKickBoxers + parseObject.get("name") + "\n";
-                        }
-
-                        FancyToast.makeText(SignUp.this, allKickBoxers,
-                                FancyToast.LENGTH_LONG, FancyToast.SUCCESS, false).show();
-
-                    }
-                } else {
+                    FancyToast.makeText(SignUp.this, parseUser.getUsername() + " is signed up",
+                            Toast.LENGTH_LONG, FancyToast.SUCCESS, false);
+                }else{
                     FancyToast.makeText(SignUp.this, e.getMessage(),
-                            FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
+                            Toast.LENGTH_LONG, FancyToast.ERROR, false);
                 }
             }
         });
     }
 
-    @OnClick(R.id.btnTransition)
-    public void transition(View v) {
-        Intent intent = new Intent(SignUp.this, SignUpLoginActivity.class);
+    @OnClick(R.id.btnGoToLogIn)
+    public void startLoginActivity(View v) {
+        Intent intent = new Intent(SignUp.this, LoginActivity.class);
         startActivity(intent);
     }
 }
