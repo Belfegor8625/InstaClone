b0fb114e0b9218793cc67aa0a38e4041bab06476
Belfegor17 <barlew94@o2.pl>
2019-02-14 17:18:08 +0100

    Rework in Login activity ImageView bug fixed

--- app/src/main/java/com/bartoszlewandowski/instaclone/LoginActivity.java
+++ app/src/main/java/com/bartoszlewandowski/instaclone/LoginActivity.java
@@ -1,13 +1,88 @@
 package com.bartoszlewandowski.instaclone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.shashank.sony.fancytoastlib.FancyToast;
+
+import java.util.Objects;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.edtLoginEmail)
+    EditText edtLoginEmail;
+    @BindView(R.id.edtLoginPassword)
+    EditText edtLoginPassword;
+    @BindView(R.id.btnLogin)
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        if (ParseUser.getCurrentUser() != null) {
+            ParseUser.logOutInBackground();
+        }
+        edtLoginPassword.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
+                    login(btnLogin);
+                }
+                return false;
+            }
+        });
+    }
+
+
+    @OnClick(R.id.btnLogin)
+    public void login(View v) {
+        if (edtLoginEmail.getText().toString().equals("") ||
+                edtLoginPassword.getText().toString().equals("")) {
+            ParseUser.logInInBackground(edtLoginEmail.getText().toString(), edtLoginPassword.getText().toString(), new LogInCallback() {
+                @Override
+                public void done(ParseUser user, ParseException e) {
+                    if (user != null && e == null) {
+                        FancyToast.makeText(LoginActivity.this, user.getUsername() + " logged in",
+                                Toast.LENGTH_LONG, FancyToast.SUCCESS, false);
+                    } else {
+                        FancyToast.makeText(LoginActivity.this, e.getMessage(),
+                                Toast.LENGTH_LONG, FancyToast.ERROR, false);
+                    }
+                }
+            });
+        }
+    }
+
+    @OnClick(R.id.btnGoToSignUp)
+    public void startSignUpActivity(View v) {
+        Intent intent = new Intent(LoginActivity.this, SignUp.class);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.rootLoginLayout)
+    public void rootLoginLayoutClick(View v) {
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        try {
+            inputMethodManager.hideSoftInputFromWindow(Objects.requireNonNull(getCurrentFocus()).getWindowToken(), 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/bartoszlewandowski/instaclone/SignUp.java
+++ app/src/main/java/com/bartoszlewandowski/instaclone/SignUp.java
@@ -1,9 +1,13 @@
 package com.bartoszlewandowski.instaclone;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -11,6 +15,8 @@ import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
+
+import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,11 +26,13 @@ public class SignUp extends AppCompatActivity {
 
     //butterknife implementation
     @BindView(R.id.edtSignUpEmail)
-    EditText edtEmail;
+    EditText edtSignUpEmail;
     @BindView(R.id.edtSignUpUsername)
-    EditText edtUsername;
+    EditText edtSignUpUsername;
     @BindView(R.id.edtSignUpPassword)
-    EditText edtPassword;
+    EditText edtSignUpPassword;
+    @BindView(R.id.btnSignUp)
+    Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,32 +40,62 @@ public class SignUp extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
         ButterKnife.bind(this);
+        edtSignUpPassword.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
+                    btnSignUpClick(btnSignUp);
+                }
+                return false;
+            }
+        });
     }
 
 
     @OnClick(R.id.btnSignUp)
     public void btnSignUpClick(View v) {
         final ParseUser parseUser = new ParseUser();
-        parseUser.setEmail(edtEmail.getText().toString());
-        parseUser.setUsername(edtUsername.getText().toString());
-        parseUser.setPassword(edtPassword.getText().toString());
-        parseUser.signUpInBackground(new SignUpCallback() {
-            @Override
-            public void done(ParseException e) {
-                if (e == null) {
-                    FancyToast.makeText(SignUp.this, parseUser.getUsername() + " is signed up",
-                            Toast.LENGTH_LONG, FancyToast.SUCCESS, false);
-                }else{
-                    FancyToast.makeText(SignUp.this, e.getMessage(),
-                            Toast.LENGTH_LONG, FancyToast.ERROR, false);
+        parseUser.setEmail(edtSignUpEmail.getText().toString());
+        parseUser.setUsername(edtSignUpUsername.getText().toString());
+        parseUser.setPassword(edtSignUpPassword.getText().toString());
+        if (edtSignUpEmail.getText().toString().equals("") ||
+                edtSignUpUsername.getText().toString().equals("") ||
+                edtSignUpPassword.getText().toString().equals("")) {
+            FancyToast.makeText(SignUp.this, "Email, Username and Password is required!",
+                    Toast.LENGTH_LONG, FancyToast.INFO, false).show();
+        } else {
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setMessage("Signing up " + parseUser.getUsername());
+            progressDialog.show();
+            parseUser.signUpInBackground(new SignUpCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e == null) {
+                        FancyToast.makeText(SignUp.this, parseUser.getUsername() + " is signed up",
+                                Toast.LENGTH_LONG, FancyToast.SUCCESS, false).show();
+                    } else {
+                        FancyToast.makeText(SignUp.this, e.getMessage(),
+                                Toast.LENGTH_LONG, FancyToast.ERROR, false).show();
+                    }
+                    progressDialog.dismiss();
                 }
-            }
-        });
+            });
+        }
     }
 
     @OnClick(R.id.btnGoToLogIn)
     public void startLoginActivity(View v) {
         Intent intent = new Intent(SignUp.this, LoginActivity.class);
         startActivity(intent);
+    }
+
+    @OnClick(R.id.rootSignUpLayout)
+    public void rootLayoutTapped(View v){
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        try{
+            inputMethodManager.hideSoftInputFromWindow(Objects.requireNonNull(getCurrentFocus()).getWindowToken(),0);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
